86dbfc9aa63f6242affefd7a6af08999915a41fa
amin <amenfd@gmail.com>
2020-04-18 16:01:47 +0430

    use absolute path in pdf_exporter.py

--- exporter/pdf_exporter.py
+++ exporter/pdf_exporter.py
@@ -1,6 +1,11 @@
+import os
+import pathlib
+
 from fpdf import FPDF
 
 from exporter.abstracts import Exporter
+
+HERE = pathlib.Path(__file__).parent
 
 
 class PDFExporter(Exporter):
@@ -8,7 +13,7 @@ class PDFExporter(Exporter):
         font_size = kwargs.get('font_size', 12)
         pdf = FPDF()
         pdf.add_page()
-        pdf.add_font('Onuava', '', './assets/fonts/onuava__.ttf', uni=True)
+        pdf.add_font('Onuava', '', os.path.join(HERE, '../assets/fonts/onuava__.ttf'), uni=True)
         pdf.set_font('Onuava', size=font_size)
         pdf.multi_cell(0, 10, content)
         pdf.output(self.path, 'F')
